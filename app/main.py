3839a06a9bf28ff37a69ed02863b1624d5b78c41
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-08-31 12:52:25 +0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept()  # wait for client
     client, addr = server_socket.accept()  # wait for client
-    client.send(b"+PONG\r\n")
+    client.send("+PONG\r\n")
 
 
 if __name__ == "__main__":
